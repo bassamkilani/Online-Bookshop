bd2001868f6b61312bad29eccc5d6c82563e5225
Bassam Kilani <bassamtzaid@gmail.com>
2019-04-20 14:40:42 +0300

    addbook

--- js/myScript.js
+++ js/myScript.js
@@ -3,7 +3,7 @@ function createAcc() {
 }
 
 // Get the modal
-var modal = document.getElementById("id01");
+var modal = document.getElementById("addBook");
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
