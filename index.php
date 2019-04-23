875c78454d089a90ab9055fb0d48abbf8c1c952e
Bassam Kilani <bassamtzaid@gmail.com>
2019-04-23 22:26:11 +0300

    commented sidebar

--- index.php
+++ index.php
@@ -165,7 +165,7 @@
               <a id="signin" class="btn btn-lg" onclick="document.getElementById('id01').style.display='block'">Signin</a>
             </li>
 
-            <li style="width: 20%; margin-left: 20px; border: none;" class="dropdown">
+            <!-- <li style="width: 20%; margin-left: 20px; border: none;" class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown">Iasmani Pinazo <span class="glyphicon glyphicon-user pull-right"></span></a>
               <ul class="dropdown-menu">
                 <li><a href="#">Account Settings <span class="glyphicon glyphicon-cog pull-right"></span></a></li>
@@ -178,7 +178,7 @@
                 <li class="divider"></li>
                 <li><a href="#">Sign Out <span class="glyphicon glyphicon-log-out pull-right"></span></a></li>
               </ul>
-            </li>
+            </li> -->
           </ul>
         </div>
       </nav>
